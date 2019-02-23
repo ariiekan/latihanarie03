7c742bfdf568de07f8d4e91ac9f4bfb45ce0ca79
kid49 <marcoroland92@yahoo.com>
2019-02-23 22:37:48 +0700

    nambah file

---
+++ latihan1.py
@@ -0,0 +1,19 @@
+import random
+
+N = int(input("Masukan Nilai N :"))
+a = 0
+for i in range(N):
+        a +=1
+        N = random.uniform(0.0,0.5)
+        print('Data ke : ',a, '==>' ,N)
+print("---SELESAI---")
+print ('')
+jawab = "lanjutkan"
+hitung = 0
+while jawab == "lanjutkan " :
+        hitung +=1
+        if jawab == "lanjutkan":
+                jawab = input("Ingin mengulang? (lanjut/tidak) ?")
+                break
+print("Total perulangan  :" + str(hitung))
+
